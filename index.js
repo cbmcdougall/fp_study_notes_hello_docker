80f97d43c78480c96c601adedd5c8cfef1fd5ff5
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-09-09 01:22:29 +0000

    Making a change to the code :)

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ui = require('readline').createInterface({
 ui.question('Hi there! What is your name?\n\n', input => {
     const message = sayHello(input)
     console.log(message)
+    console.log('This is an edit to the code :)')
     ui.close();
 })
 
